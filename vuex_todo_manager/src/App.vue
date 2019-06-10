818bb794c10b17330932ba0845705e081d8596bc
Josh Silverman <jrsilverman71@hotmail.com>
2019-06-10 11:01:12 -0700

    setting up vuex app

---
+++ vuex_todo_manager/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Todos/>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+
+export default {
+  name: "app",
+  components: {
+    Todos
+  }
+};
+</script>
+
+<style>
+</style>
